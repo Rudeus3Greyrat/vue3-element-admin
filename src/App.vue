712007f271af4f28aca1b6eb9268522d16ef78c9
梅子文 <meiziwen@kds-china.com>
2021-01-06 15:41:34 +0800

    feat(all)基本实现正常运行。目前components部分因为依赖包未更新至Vue3的缘故无法正常运行。

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,50 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import store from './store'
+import router from './router'
 
-createApp(App).mount('#app')
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import './styles/element-variables.scss'
+import enLang from 'element-plus/lib/locale/lang/en'// 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
+
+import '@/styles/index.scss' // global css
+
+import './icons' // icon
+import './permission' // permission control
+import './utils/error-log' // error log
+
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
+const app = createApp(App)
+
+app.config.productionTip = false
+
+// register globally
+app.component('SvgIcon', SvgIcon)
+app.use(ElementPlus, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  locale: enLang // 如果使用中文，无需设置，请删除
+})
+app.use(store)
+app.use(router)
+app.mount('#app')
+
+export default app
